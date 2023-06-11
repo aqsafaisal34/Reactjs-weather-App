9f5b833be8d1fc384276f80abb695188ae6f3787
aqsafaisal34 <aqsay.faisal34@gmail.com>
2023-06-11 18:11:19 +0500

    work done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [cityName, setCityName] = useState("");
+  const [city, setcity] = useState("");
+  const [currentTemp, setCurrentTemp] = useState("");
+  const [description, setDescription] = useState("");
+  const [mintemp, setminTemp] = useState("");
+  const [maxtemp, setmaxTemp] = useState("");
+
+  
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=0336380060bfd5eb16732502b38f1fcb&units=metric`
+      )
+      .then((response) => {
+        console.log("response: ", response.data);
+
+        setcity(response.data.name);
+        setCurrentTemp(Math.round(response.data.main.temp));
+        setDescription(response.data.weather[0].description);
+        setminTemp(response.data.main.temp_min);
+        setminTemp(Math.round(response.data.main.temp_min));
+        setmaxTemp(Math.round(response.data.main.temp_max));
+        
+
+      })
+      .catch((err) => {
+        console.log("error: ", err);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="weatherApp">
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            required
+            placeholder="Enter your city name"
+            onChange={(e) => {
+              setCityName(e.target.value);
+            }}
+          />
+          <button type="submit">Check</button>
+        </form>
+        {
+       
+       (currentTemp && mintemp && maxtemp === null) ? null :
+      <div>
+       <div id="cityName">{city}</div>
+       <div id="currentTemp">{(currentTemp)}°C</div>
+       <div id="description">{description}</div>
+       <div id="min-max">
+       <div>{(mintemp)}°C /</div>
+       <div>{(maxtemp)}°C</div>
+       </div>
+       </div>
+
+        }
+      </div>
     </div>
   );
 }
